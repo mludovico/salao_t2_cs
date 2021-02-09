1fd79ceb1ca6281605b7f3d85efc52337f1e0479
marcelo <marcelo@hexplore.com>
2021-02-08 21:46:00 -0300

    Create api project

---
+++ SalaoT2.Api/Controllers/FuncionarioController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalaoT2.Dominio;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SalaoT2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FuncionarioController : Controller
+    {
+        private readonly MinhaBaseFuncionarios baseFuncionarios;
+
+        public FuncionarioController()
+        {
+            baseFuncionarios = new MinhaBaseFuncionarios();
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Funcionario> Get()
+        {
+            return baseFuncionarios.Funcionarios;
+        }
+
+        // GET api/values/5
+        [HttpGet("{matricula}")]
+        public Funcionario Get(int matricula)
+        {
+            return baseFuncionarios.Funcionarios.FirstOrDefault(item => item.Matricula == matricula);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IEnumerable<Funcionario> Post([FromBody] Funcionario funcionario)
+        {
+            baseFuncionarios.Incluir(funcionario);
+            return baseFuncionarios.Funcionarios;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IEnumerable<Funcionario> Put(int id, [FromBody] Funcionario funcionario)
+        {
+            baseFuncionarios.AlterarUmFuncionario(id, funcionario.Nome,
+                funcionario.Telefone, funcionario.Endereco, funcionario.Cargo);
+            return baseFuncionarios.Funcionarios;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SalaoT2.Api/Controllers/ServicoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalaoT2.Dominio;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SalaoT2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ServicoController : Controller
+    {
+        private readonly MinhaBaseServicos baseServicos;
+
+        public ServicoController()
+        {
+            baseServicos = new MinhaBaseServicos();
+        }
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Servico> Get()
+        {
+            return baseServicos.Servicos;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Servico Get(int id)
+        {
+            return baseServicos.ConsultarPorId(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IEnumerable<Servico> Post([FromBody] Servico servico)
+        {
+            baseServicos.Incluir(servico);
+            return baseServicos.Servicos;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IEnumerable<Servico> Put(int id, [FromBody] Servico servico)
+        {
+            baseServicos.AlterarUmServico(id, servico.Nome, servico.MinutosParaExecucao, servico.Preco);
+            return baseServicos.Servicos;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
